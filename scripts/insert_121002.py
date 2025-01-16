9259bbced53d9a877de3d4cd5fffe7e33f9e622b
Ilyosjon Nishanov <nishanovii@risk-python1.ipa.ipotekabank.uz>
2025-01-16 16:10:58 +0500

    changes i dont know

--- scripts/insert_121002.py
+++ scripts/insert_121002.py
@@ -5,7 +5,7 @@ from multiprocessing import Pool, cpu_count
 from fetch_and_write_data_121002 import fetch_and_write_data
 
 print(f"Start time: {datetime.now()}")
-table_name = '[BRONZE].[121002_test]'
+table_name = '[BRONZE].[121002]'
 date_ranges = generate_date_ranges(2023, 2025, 'month')
 
 with Pool(cpu_count()) as pool:
