2de1b2812d1e50a9e59bd5d2140baca7d2d2e4aa
Ilyosjon Nishanov <nishanovii@risk-python1.ipa.ipotekabank.uz>
2025-01-24 11:22:05 +0500

    added backup

---
+++ scripts/backup_121002.py
@@ -0,0 +1,9 @@
+from my_connections import get_mssql_connection
+
+sql_connection = get_mssql_connection()
+sql_cursor = sql_connection.cursor()
+query = f"""insert into bronze.[121002_backup] 
+select *, cast(getdate() as date) as DATE_MODIFIED from bronze.[121002]"""
+sql_cursor.execute(query)
+sql_connection.commit()
+sql_connection.close()
