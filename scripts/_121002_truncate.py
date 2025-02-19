2c7ff4f95a2aee8df466ea30e874becd9b675cdf
Ilyosjon Nishanov <nishanovii@risk-python1.ipa.ipotekabank.uz>
2025-02-19 17:28:06 +0500

    truncate query was corrected

--- scripts/_121002_truncate.py
+++ scripts/_121002_truncate.py
@@ -2,6 +2,6 @@ from _121002_connections import get_mssql_connection
 
 sql_connection = get_mssql_connection()
 sql_cursor = sql_connection.cursor()
-sql_cursor.execute('truncate table bronze.bulk_insert_test')
+sql_cursor.execute('truncate table bronze.[121002]')
 sql_connection.commit()
 sql_connection.close()
